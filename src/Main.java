1cc56da2578821634dc509eac3e90935e891ca9b
mouta-a <moutasem.a.h@live.se>
2022-11-09 13:08:22 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Support support = new Support();
+        System.out.println("Hello world!");
+        support.printMessage();
+
+    }
+}
---
+++ src/Support.java
@@ -0,0 +1,7 @@
+public class Support {
+
+
+    public void printMessage(){
+        System.out.println("This message is run from another class: 'Support'");
+    }
+}
