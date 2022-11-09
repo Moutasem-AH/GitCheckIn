472acbbd9b51afed4f7ab5c3a221e29a95f807b1
mouta-a <moutasem.a.h@live.se>
2022-11-09 13:15:04 +0100

    added new class

---
+++ src/Helper.java
@@ -0,0 +1,8 @@
+
+public class Helper {
+
+    public void PrintBranchMessage(){
+        System.out.println("This is a method created on a new branch");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Support support = new Support();
+        Helper helper = new Helper();
+
         System.out.println("Hello world!");
         support.printMessage();
+        helper.PrintBranchMessage();
 
     }
 }
